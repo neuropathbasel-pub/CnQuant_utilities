f790a3c64d06757604c01bdc93e4d02716b620be
benmacfre <benjamin.freyter@gmail.com>
2025-04-16 15:17:13 +0200

    updated send_crash_email to send mesages to many receipients ot once

--- cnv_apps_utilities/crash_report.py
+++ cnv_apps_utilities/crash_report.py
@@ -2,13 +2,13 @@ import smtplib
 from email.mime.text import MIMEText
 from datetime import datetime
 
-def send_crash_email(error_message: str, sender: str, receiver: str, password: str, app_name: str):
+def send_crash_email(error_message: str, sender: str, receivers: list[str], password: str, app_name: str):
     """Sends an email notification when an application crashes.
 
     Args:
         error_message (str): The error message or stack trace detailing the crash.
         sender (str): The email address sending the notification (e.g., 'your_email@gmail.com').
-        receiver (str): The email address receiving the notification.
+        receiver (list[str]): The list of email addresses receiving the notification.
         password (str): The password or app-specific password for the sender's email account.
         app_name (str): The name of the application that crashed, included in the email.
 
@@ -20,16 +20,16 @@ def send_crash_email(error_message: str, sender: str, receiver: str, password: s
         - Requires the sender's email to allow less secure apps or an app-specific password.
         - Includes a timestamp in the format 'YYYY-MM-DD HH-MM-SS' in the subject and body.
     """
-    timestamp = datetime.now().strftime(format='%Y-%m-%d %H-%M-%S')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
     msg = MIMEText(f"{app_name} crashed with error:\n\n{error_message} on {timestamp}")
     msg["Subject"] = f"{app_name} crashed on {timestamp}"
     msg["From"] = sender
-    msg["To"] = receiver
+    msg["To"] = ", ".join(receivers)
 
     try:
         with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
             server.starttls()
             server.login(user=sender, password=password)
-            server.sendmail(from_addr=sender, to_addrs=receiver, msg=msg.as_string())
+            server.sendmail(from_addr=sender, to_addrs=receivers, msg=msg.as_string())
     except Exception as e:
         print(f"Failed to send email: {e}")
